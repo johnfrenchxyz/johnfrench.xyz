66cbd61d75ed3d56a4293f4a4a01f90db1c16646
John French <johnwesleyfrench@gmail.com>
2016-06-07 08:30:38 -0400

    Fixed nav margins on small screens and added x toggle (still need to toggle back though)

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 $(document).foundation();
-$(document).ready(function(){
-   $('#hamburger').click(function(){
-      $( "nav" ).slideToggle( "fast", function() {
-    // Animation complete.
-  });
+$(document).ready(function() {
+   $('#hamburger').click(function() {
+      $("nav").slideToggle("fast", function() {});
+      $("#hamburger").replaceWith('<a id="hamburger"><i class="material-icons hamburger-icon">clear</i></a>');
    });
 });
