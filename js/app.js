965860f497edc95b56d8e8c145bc94b62902c1cb
John French <johnwesleyfrench@gmail.com>
2016-06-08 16:57:54 -0400

    Added portfolio page

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
 $(document).foundation();
-$(document).ready(function() {
-   $('#hamburger').click(function() {
-      $("nav").slideToggle("fast", function() {});
-      // Icon Toggle
-      if ( $('#hamburger i').text() === 'menu' ) {
-         $('#hamburger i').text('clear');
-      } else {
-         $('#hamburger i').text('menu');
-      }
-   });
+
+// Main Navigation Toggle Function
+var menuToggle = function() {
+   $("nav").slideToggle("fast", function() {});
+   // Icon Toggle
+   if ($('#hamburger i').text() === 'menu') {
+      $('#hamburger i').text('clear');
+   } else {
+      $('#hamburger i').text('menu');
+   }
+};
+// Main Navigation Toggle (On Mouse Click)
+$('#hamburger').click(function() {
+   menuToggle();
+});
+// Main Navigation Toggle (On Enter)
+$("#hamburger").keyup(function(event) {
+   if (event.keyCode == 13) {
+      menuToggle();
+   }
+});
+
+
+// Portfolio Item Toggle Function
+$('.portfolio-item-header').click(function(){
+   $(this).next('.portfolio-item-body').slideToggle("fast");
+
 });
